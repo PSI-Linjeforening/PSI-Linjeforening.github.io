939106d35ab07edf7ea5a9af0936c96e8208b676
Kristian <kristianint@outlook.com>
2020-02-10 23:45:33 +0100

    oppdater om og pr gruppe side

---
+++ src/routes/grupper/pr.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Facebook from "svelte-icons/io/IoLogoFacebook.svelte";
+	import Camera from "svelte-icons/fa/FaCameraRetro.svelte";
+</script>
+
+<svelte:head>
+	<title>PSing</title>
+</svelte:head>
+
+<h1 class="icon">
+	PR
+	<Camera />
+</h1>
+
+<p>
+	PR gruppen er ansvarlig for PSI sine offentlige relasjoner, samt driftingen
+	av sider som
+	<a href="https://www.facebook.com/psi.linjeforening/">
+		PSI sin facebookside
+	</a>
+	og
+	<a href="https://www.facebook.com/fnkyfreud/">Fnky Freud.</a>
+</p>
--- src/routes/om.svelte
+++ src/routes/om.svelte
@@ -1,30 +1,110 @@
+<script>
+	import Mail from "svelte-icons/go/GoMail.svelte";
+	import Map from "svelte-icons/fa/FaMapMarkedAlt.svelte";
+	import Messenger from "svelte-icons/fa/FaFacebookMessenger.svelte";
+	import Facebook from "svelte-icons/io/IoLogoFacebook.svelte";
+	import Snapchat from "svelte-icons/fa/FaSnapchatSquare.svelte";
+</script>
+
+<style>
+	h2,
+	h3 {
+		margin-top: 1em;
+	}
+	ul {
+		margin: 0;
+	}
+</style>
+
 <svelte:head>
 	<title>Om</title>
 </svelte:head>
 
 <h1>Om PSI</h1>
 
-<h3>Hvem er vi?</h3>
-
 <p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ab nam illum
-	voluptas hic odio alias minus deleniti, vero voluptatum soluta qui rem
-	excepturi dolorem accusamus sit nesciunt at. Aspernatur.
+	PSI er linjeforeningen for psykologi årsstudium og bachelor ved NTNU.
+	Linjeforeningens formål er å samle psykologistudentene til faglige og
+	sosiale arrangementer!
 </p>
 
-<h3>Hvem er vi for?</h3>
+<h3>STYRET 2019/2020:</h3>
 
+<ul>
+	<li>
+		<b>Leder:</b>
+		Svein Fredrik Foshaugen Nyvoll
+	</li>
+	<li>
+		<b>Nestleder:</b>
+		Helene Hagen
+	</li>
+	<li>
+		<b>Økonomiansvarlig:</b>
+		Emma Birkelund Bendiksen
+	</li>
+	<li>
+		<b>PR-ansvarlig:</b>
+		Maria Syrtveit
+	</li>
+	<li>
+		<b>Eventansvarlig:</b>
+		Nora Sofie Trohaug
+	</li>
+	<li>
+		<b>Aktivitetsansvarlig:</b>
+		Stian Søreng
+	</li>
+</ul>
+
+<h2>Fnky Freud</h2>
 <p>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni iste,
-	quibusdam dolor voluptates neque praesentium debitis! Necessitatibus alias
-	quam est ab, esse maiores voluptates praesentium nemo quidem, laudantium
-	dicta.
+	Fnky Freud poster mye info fra PSI og kan følges på både facebook og
+	snapchat:
+</p>
+<p>
+	<a class="icon medium-big" href="https://www.facebook.com/fnkyfreud/">
+		<Facebook title="Fnky Freud på Facebook" />
+	</a>
+	<a
+		class="icon medium-big"
+		href="https://www.facebook.com/messages/t/fnkyfreud/">
+		<Messenger title="Fnky Freud på Messenger" />
+	</a>
+	<span class="icon medium-big">
+		<Snapchat title="Fnky Freud på Snapchat" />
+	</span>
+	<span class="medium-big">psi.ntnu</span>
 </p>
 
-<h3>Hva kan du gjøre i PSI?</h3>
+<h2>Kontakt Oss:</h2>
+<p>
+	<a class="icon" href="https://m.me/psi.linjeforening">
+		<Messenger title="PSI på Messenger" />
+		PSI
+	</a>
+</p>
 
 <p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus eveniet
-	qui culpa perspiciatis hic, quia aliquid, rem, aperiam rerum doloremque
-	harum veniam deleniti? Iusto, excepturi dolor unde maxime ullam provident!
+	<a class="icon" href="mailto:psi.linjeforening@gmail.com">
+		<Mail title="Mail PSI" />
+		psi.linjeforening@gmail.com
+	</a>
+</p>
+<p>
+	Eventkomitén:
+	<a class="icon" href="mailto:psi.eventkom@gmail.com">
+		<Mail title="Mail Eventkomitén" />
+		psi.eventkom@gmail.com
+	</a>
+</p>
+
+<p>
+	Vi har også kontor på rom 12409, Dragvoll.
+	<a
+		class="icon"
+		href="https://use.mazemap.com/#v=1&center=10.469975,63.407166&zoom=18&sharepoitype=poi&sharepoi=92294&zlevel=2&campusid=18">
+		<Map title="Kontoret til PSI på MazeMap" />
+		MazeMap
+	</a>
 </p>
